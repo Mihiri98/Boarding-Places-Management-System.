1f697ea9e290aeb4d3c3b78e4dd15bdd3c25a7bf
@mihiri_madurangi <139603968+Mihiri98@users.noreply.github.com>
2025-01-17 05:15:38 +0000

    add house php updated

--- addhouse.php
+++ addhouse.php
@@ -10,24 +10,12 @@ $state=$_GET['s'];
 $city=$_GET['city'];
 $add=$_GET['a'];
 $desc=$_GET['de'];
-/*$folder="house/";
-$_FILES["p"];
-$pics= "images/".basename($_FILES['p']['name']);*/
+
 
 if($_GET['submit'])
 {
 	if($ownerid!="" && $nrooms!="" && $desc!="")
 	{
-     // $pics = addslashes(file_get_contents($_FILES["p"]["tmp_name"]));
-     /*
-//you keep your column name setting for insertion. I keep image type Blob.
-$query = "INSERT INTO products (id,image) VALUES('','$image')";  
-$qry = mysqli_query($conn, $query);*/
-
-//$image = addslashes(file_GET_contents($_FILES['$pics']));
-
-
-
 		$query="insert into house(owner_id,no_of_rooms,rate,pics,country,state,city,address,description) values('$ownerid','$nrooms','$rate','{$pics}','$country','$state','$city','$add','$desc')";
 		$data=mysqli_query($conn,$query);
 		
