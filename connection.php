d0ff8a0964445a664ee59faef1db708a3fe2bced
@mihiri_madurangi <139603968+Mihiri98@users.noreply.github.com>
2025-01-17 05:20:59 +0000

    connection php updated

--- connection.php
+++ connection.php
@@ -5,9 +5,7 @@ $password="";
 $dbname="01 login details & project info.txt";
 $conn= mysqli_connect($servername,$username,$password,$dbname);
 if(!$conn)
-
 {
 	die("Conection failed because".mysqli_connect_error());
 }
-
 ?>
